8a84145b2ad4d5bf55446f0aed750bdb1e30bd80
Shingo Sato <shinsugar@gmail.com>
2016-10-16 17:27:42 +0900

    Add more tests

--- test.js
+++ test.js
@@ -12,6 +12,16 @@ test('boundCompose', t => {
   t.is(r, 2985)
 })
 
+test('boundCompose arg length 1', t => {
+  const r = boundCompose(add)(10)(1,2,3,4,5)
+  t.is(r, 25)
+})
+
+test('boundCompose arg length 0', t => {
+  const r = boundCompose()(10)(1,2,3,4,5)
+  t.is(r, 10)
+})
+
 test('boundComposeLeft', t => {
   const f = boundCompose.left(add, multi, sub)(10)
   const r = boundCompose.left(add, multi, sub)(10)(1,2,3,4,5)
